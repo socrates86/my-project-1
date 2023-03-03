7eb9c4d269653564bb39928e94fb732921257d63
socrates86 <morrisonikechukwu86@gmail.com>
2023-03-03 21:43:02 +0100

    initial commit

---
+++ src/component/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+
+export default function Footer () {
+    return (
+      <footer className="footer">
+        <small className="small">(c) 2022  Morrison development. All rights reserved.</small>
+      </footer>
+    )
+  }
